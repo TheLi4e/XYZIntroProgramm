6dd0d842f6faa712f2317a67354e28793b63aebe
TheLi4e <li4e99@gmail.com>
2024-01-03 15:00:26 +0200

    Добавьте файлы проекта.

---
+++ Assets/HW1.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HW1 : MonoBehaviour
+{
+
+    void Start()
+    {
+        Homework1();
+    }
+
+    void Homework1()
+    {
+        Debug.Log(1675 * 0.24);
+    }
+}
